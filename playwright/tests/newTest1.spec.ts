0bc42d8d5ee9ee2964c8b4779cc95f331b170823
Vlad <vovaukr96@gmail.com>
2026-07-28 13:37:25 +0300

    Add price sorting test, parameterized add-to-cart tests

--- playwright/tests/newTest1.spec.ts
+++ playwright/tests/newTest1.spec.ts
@@ -12,3 +12,19 @@ test('add item to cart', async ({ loginPage, productsPage }) => {
   await productsPage.addToCart('Sauce Labs Backpack');
   expect(await productsPage.getCartCount()).toBe('1');
 });
+
+test('sort products by price low to high', async ({ loginPage, productsPage }) => {
+  await productsPage.sortBy('lohi');
+  const prices = await productsPage.getAllPrices();
+  const sortedPrices = [...prices].sort((a, b) => a - b);
+  expect(prices).toEqual(sortedPrices);
+});
+
+const productsToTest = ['Sauce Labs Backpack', 'Sauce Labs Bike Light', 'Sauce Labs Bolt T-Shirt'];
+
+for (const productName of productsToTest) {
+    test(`add ${productName} to cart`, async ({ loginPage, productsPage }) => {
+        await productsPage.addToCart(productName);
+        expect(await productsPage.getCartCount()).toBe('1');
+    });
+}
